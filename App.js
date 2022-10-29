15cf888c5cd1b1bacfd331b565d9e910618ddfb1
ManojKrJena <apumanoj@gmail.com>
2022-10-29 12:23:55 +0530

    change in html part

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-
+let icons = document.getElementsByTagName("a");
+for(let i=0; i<icons.length; i++){
+  icons[i].addEventListener("click", (event) => {
+    let activeElement = document.getElementsByClassName("active")[0];
+    activeElement.classList.remove("active");
+    icons[i].classList.add("active");
+  });
+}
